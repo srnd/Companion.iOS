b6f9ab3ad0999e06197f317f5053c90e39213a1b
TJ Horner <me@tjhorner.com>
2018-06-16 10:33:14 -0700

    Initial Commit

---
+++ CodeDay Companion/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CodeDay Companion
+//
+//  Created by TJ Horner on 6/16/18.
+//  Copyright © 2018 srnd.org. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
